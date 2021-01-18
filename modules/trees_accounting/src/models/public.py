6940d8d5139a274b1a61c49bc550a0943fd0ad6d
Evgenii Omelchuk <omelchuk_evgenii@mail.ru>
2021-01-18 15:27:30 +0300

    fix bug Исправлена работа с обновлённой БД

--- modules/trees_accounting/src/models/public.py
+++ modules/trees_accounting/src/models/public.py
@@ -15,13 +15,12 @@ class Area(BaseModel):
     uuid = CharField(column_name="uid")
     num_forestry = IntegerField(column_name="num_lch", help_text="Номер лесничества")
     compartment = CharField(column_name="num_kv", help_text="Номер квартала")
-    sub_compartment = CharField(column_name="num_vd", help_text="Номер выдела")
+    sub_compartment = CharField(column_name="num_vds", help_text="Номер выдела")
     area = DecimalField(column_name="area", help_text="Экспл. площадь")
     num_enterprise = IntegerField(column_name="leshos", help_text="Номер лесхоза")
     num_cutting_area = CharField(column_name="num", help_text="Номер лесосеки")
     use_type = CharField(column_name="usetype", help_text="Тип пользования")
     cutting_type = CharField(column_name="cuttingtyp", help_text="Тип рубки")
-    num_plot = CharField(column_name="plot", help_text="Делянка")
     person_name = CharField(column_name="fio", help_text="ФИО работника")
     date_trial = CharField(
         column_name="date",
@@ -29,7 +28,6 @@ class Area(BaseModel):
         default=date.today().strftime("%d.%m.%Y"),
     )
     description = CharField(column_name="info", help_text="Доп. информация")
-    num_vds = CharField(column_name="num_vds")
     leshos_text = CharField(column_name="leshos_text")
     lesnich_text = CharField(column_name="lesnich_text")
 
