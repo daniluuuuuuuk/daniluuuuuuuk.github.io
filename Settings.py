6781c44096722117e9879d0d0e0d62db2be441ee
Evgenii Omelchuk <omelchuk_evgenii@mail.ru>
2021-04-20 14:50:07 +0300

    — Удалена часть для работы с BT дальномером

--- Settings.py
+++ Settings.py
@@ -4,7 +4,6 @@ from .tools import config
 from .tools.ImportDatabase import DataImport
 from .tools import module_errors as er
 from .PostgisDB import PostGisDB
-from .BluetoothAdapter import BTAdapter
 from PyQt5 import QtCore
 from .modules.otvod.tools.threading.ForestObjectWorker import (
     Worker as ForestObjWorker,
@@ -69,7 +68,6 @@ class SettingsController(QtCore.QObject):
         self.populateEnterprise()
 
         self.populateBDSettings()
-        self.populateBTSettings()
 
         self.tableUi.saveConfigButton.clicked.connect(self.saveBDConfig)
         self.tableUi.importDB_pushButton.clicked.connect(self.importDB)
@@ -78,14 +76,6 @@ class SettingsController(QtCore.QObject):
         )
         self.tableUi.saveEnterpriseSettingsButton.clicked.connect(
             self.saveEnterpriseConfig
-        )
-
-        self.tableUi.changeForkComButton.clicked.connect(self.changeComPort)
-        self.tableUi.changeRangeFinderComButton.clicked.connect(
-            self.changeComPort
-        )
-        self.tableUi.SaveBTConfigPushButton.clicked.connect(
-            self.saveComPortConfig
         )
 
         self.tableUi.gplho_comboBox.currentTextChanged.connect(
@@ -296,48 +286,6 @@ class SettingsController(QtCore.QObject):
             self.tableUi.BDNameLineEdit.setText(
                 bdsettings.get("database", "No data")
             )
-        except Exception as e:
-            QMessageBox.information(
-                None, er.MODULE_ERROR, er.CONFIG_FILE_ERROR + str(e)
-            )
-
-    def populateBTSettings(self):
-        cf = config.Configurer("bluetooth")
-        btsettings = cf.readConfigs()
-        self.tableUi.forkComPortlineEdit.setText(
-            btsettings.get("fork", "No data")
-        )
-        self.tableUi.rangeFinderLineEdit.setText(
-            btsettings.get("rangefinder", "No data")
-        )
-
-    def changeComPort(self, *args, **kwargs):
-        sender = self.sd.sender()
-
-        sd = comPortWindow()
-        ui = sd.ui
-
-        btAdapter = BTAdapter()
-        ui.comPortCombobox.addItems(
-            [x.device for x in btAdapter.getAvailablePorts()]
-        )
-        result = sd.exec()
-
-        if result == QDialog.Accepted:
-            port = ui.comPortCombobox.currentText()
-            if sender == self.tableUi.changeForkComButton:
-                self.tableUi.forkComPortlineEdit.setText(port)
-            elif sender == self.tableUi.changeRangeFinderComButton:
-                self.tableUi.rangeFinderLineEdit.setText(port)
-
-    def saveComPortConfig(self):
-        try:
-            settingsDict = {
-                "fork": self.tableUi.forkComPortlineEdit.text(),
-                "rangefinder": self.tableUi.rangeFinderLineEdit.text(),
-            }
-            cf = config.Configurer("bluetooth", settingsDict)
-            cf.writeConfigs()
         except Exception as e:
             QMessageBox.information(
                 None, er.MODULE_ERROR, er.CONFIG_FILE_ERROR + str(e)
