58baa9c11354fe1f1ff7c30b77a4431c1eecd816
daniluuuuuuuk <54038074+daniluuuuuuuk@users.noreply.github.com>
2024-06-21 15:13:38 +0300

    Delete sql directory

--- sql/2.1.1.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO dictionary.xmer (code_xmer, name_xmer) VALUES(115, NULL);
-INSERT INTO dictionary.xmer (code_xmer, name_xmer) VALUES(116, NULL);
--- sql/2.2.1.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.subcompartment_taxation ADD grv int2 NULL;
-ALTER TABLE public.subcompartment_taxation_m10 ALTER COLUMN poln TYPE numeric(3,2) USING poln::numeric;
